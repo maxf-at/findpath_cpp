6182175ea8c1ee7fbd1090de9fdb84c51de0665c
Max Faissner <max.faissner@gmail.com>
2021-04-19 23:40:13 +0200

    19/04

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@
 # pybind11 compile
 # g++ ./include/findpath.cpp -o3 -std=c++20 -shared -fPIC $(python3 -m pybind11 --includes) -lRNA -lm -I./include/ -I/usr/include/python3.8 -I./usr/lib/ViennaRNA/ -DEBUG_FINDPATH -fno-lto -pthread -march=native -o findpath$(python3-config --extension-suffix) 
 clang++ ./include/findpath.cpp -o3 -std=c++20 -shared -fPIC $(python3 -m pybind11 --includes) -lRNA -I./include/ -fno-lto -pthread -march=native -o findpath$(python3-config --extension-suffix) 
+
--- launch.py
+++ launch.py
@@ -26,6 +26,7 @@ s2 =       ".............((((((((((((.(((((.((.((((((((((...((.((((((((((...(((.
 # s1 = "....((((((.......)))...)))((((..(((...((((((((...))))).))).)))...))))."
 # s2 = ".(((((((........)))..)))).((((..(((..((..(((((...))))).))..)))...))))."
 
+# this example requires suboptimal paths for merging
 # sequence = "UGAAGACCCAUUGAGUAACGACACCGCACGGCGCAUGGCGUCAGAGUAGCACUGCCUCGU"
 # s1 =       "....(((((((.(.(...((....)).....).))))).)))((.((......))))..."
 # s2 =       "....(((((((...((..((........))..)))))).))).(((..((...))))).."
@@ -49,14 +50,22 @@ s2 =       ".............((((((((((((.(((((.((.((((((((((...((.((((((((((...(((.
 # s2 = ".....(((.(((......)))))).......(((((.((((........((((..(((...((.(((((....))))).)).)))..)))))))))))))"
 
 
+# sequence = "CACAACGAGACUACUCAUGCACAUAAGGAAGGUUAUCGCCAUAGACAUGGCGCGGCAGCGCAGAAUGUUUAAAUCUAAAUCUGGUAUGGGAGGCGUGCCCGUUGGUAUGAAGAAAUUUGCUGGGAGAAAAAGUCUAAGGCCUUGAAUCCGGCGGGUCUUAAUACUUACCUACAAAAUCAUCAGGCUGUACUUCCUGUAUC"
+# s1 = "......(((....))).........((((((......(((.(((((((.((((....))))...)))))))...........((((((.....)))))).((.(((((.((((..(((((((((.....(((.(....).)))...))))))))))))).))))).))..............)))....))))))....."
+# s2 = "......(((....)))((((.....((((.(((.((.(((.(((((((...(((....)))...)))))))...........((((((.....)))))).((.(((((.((((..(((((((((.....(((((...)).)))...))))))))))))).))))).))..............))).)))))))))))))."
+
+# sequence = "UGAAGACCCAUUGAGUAAAA"
+# s1       = "(((((((.....)))).)))"
+# s2       = "(((((((.....)))).)))"
+
 # print (sequence)
 # print (s1)
 # print (s2)
 print ("~~~~~~~~~~~")
 
-search_width_multiplier = 2
+search_width_multiplier = 4
 search_width = RNA.bp_distance(s1, s2)*search_width_multiplier
-print ("search width:", search_width, "bp_dist:", RNA.bp_distance(s1, s2))
+print ("search width:", search_width, "bp_dist:", RNA.bp_distance(s1, s2), "seq_length:", len(sequence))
 
 
 
@@ -69,6 +78,8 @@ bp_dist = RNA.bp_distance(s1, s2)
 # print ("bp dist:", bp_dist, len(sequence))
 
 
+findpath.init_single_findpath(sequence, s1, s2, search_width_multiplier, True)
+
 
 start_findpath = time.time()
 result = findpath.init_single_findpath(sequence, s1, s2, search_width_multiplier, True)
@@ -78,13 +89,13 @@ print ("~~~~~~~~~~~")
 print (f'single section')
 print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
 
-start_findpath = time.time()
-result = findpath.init_merge_findpath(sequence, s1, s2, search_width_multiplier, True)
-result = result/100.0
-runtime = time.time()-start_findpath
-print ("~~~~~~~~~~~")
-print (f'merge findpath')
-print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
+# start_findpath = time.time()
+# result = findpath.init_merge_findpath(sequence, s1, s2, search_width_multiplier, True)
+# result = result/100.0
+# runtime = time.time()-start_findpath
+# print ("~~~~~~~~~~~")
+# print (f'merge findpath')
+# print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
 
 start_findpath = time.time()
 result = findpath.init_mfe_findpath(sequence, s1, s2, search_width_multiplier, True)
@@ -94,12 +105,12 @@ print ("~~~~~~~~~~~")
 print (f'merge findpath (+MFE)')
 print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
 
-start_findpath = time.time()
-result = findpath_librna.pathfinder(sequence, s1, s2, search_width=search_width_multiplier*bp_dist)
-runtime = time.time()-start_findpath
-print ("~~~~~~~~~~~")
-print (f'librna findpath (orig SWIG bindings)')
-print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
+# start_findpath = time.time()
+# result = findpath_librna.pathfinder(sequence, s1, s2, search_width=search_width_multiplier*bp_dist)
+# runtime = time.time()-start_findpath
+# print ("~~~~~~~~~~~")
+# print (f'librna findpath (orig SWIG bindings)')
+# print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
 
 start_findpath = time.time()
 result = findpath.init_vrna_findpath(sequence, s1, s2, search_width_multiplier, True)
@@ -108,3 +119,10 @@ runtime = time.time()-start_findpath
 print ("~~~~~~~~~~~")
 print (f'librna findpath (PYBIND11)')
 print (f'S: {result:2.2f}, barrier: {result-s1_eval:2.2f}, runtime: {runtime:2.4f} s')
+
+
+start_findpath = time.time()
+for i in range(10):
+    findpath.init_single_findpath(sequence, s1, s2, search_width_multiplier, True)
+runtime = time.time()-start_findpath
+print (f'runtime: {runtime:2.4f} s')
